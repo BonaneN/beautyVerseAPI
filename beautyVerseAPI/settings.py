c5a0f1200f4823f5ecf3f7a2d2337da787b90371
BonaneN <rbonane336@gmail.com>
2025-12-06 16:18:35 +0200

    apps addition

--- beautyVerseAPI/settings.py
+++ beautyVerseAPI/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appointments',
+    'artists',
+    'cart',
+    'products',
+    'services',
+    'users',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
