afec1cce781b470cd5ad2c34108ff84532d1c2f5
BonaneN <rbonane336@gmail.com>
2025-12-10 15:17:46 +0200

    register&login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+# Registration endpoint
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+# Login endpoint
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
