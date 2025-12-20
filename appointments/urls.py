92c54ea63bd39467d31ce6c417cccc097268d161
BonaneN <rbonane336@gmail.com>
2025-12-20 14:30:21 +0200

    feature: booking management and viewing

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from .views import AppointmentViewSet
 
 urlpatterns = [
-    path('book-session/', lambda r: None, name='appointment-book'),
-    path('my-appointments/', lambda r: None, name='appointment-list'),
+    path('my-appointments/', AppointmentViewSet.as_view({'get': 'my_appointments'}), name='my-appointments'),
+    path('preview-summary/', AppointmentViewSet.as_view({'post': 'preview_summary'}), name='appointment-preview'),
+    path('book-session/', AppointmentViewSet.as_view({'post': 'book_session'}), name='appointment-book'),
+    path('<int:pk>/cancel-appointment/', AppointmentViewSet.as_view({'post': 'cancel_appointment'}), name='appointment-cancel'),
+    path('<int:pk>/update-appointment/', AppointmentViewSet.as_view({'patch': 'update_appointment'}), name='appointment-update'),
 ]
